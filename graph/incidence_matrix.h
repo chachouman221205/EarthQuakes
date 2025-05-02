26b93d1c50a721b563b652d37421d9c172e471d2
chachouman <boudekhani.nassim@gmail.com>
2025-05-02 14:01:28 +0200

    incidence_matrix.h créé

---
+++ graph/incidence_matrix.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "graph.h"
+
+typedef struct Matrix {
+    int size;
+    Road** grid;
+} Matrix;
+
+Matrix* init_empty_matrix(int size);
+
+void free_matrix_content(Matrix* matrix);
+
+void free_matrix(Matrix* matrix);

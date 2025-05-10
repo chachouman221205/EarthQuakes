ebc30592cf2ae8cd48f86f3e9864a3753e9230f6
nono2945 <coupriejoseph@gmail.com>
2025-05-10 15:12:39 +0200

    a

--- test/gen_files/file_gen.c
+++ test/gen_files/file_gen.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdbool.h>
+
+#include "../../graph/graph.h"
+#include "../../graph/incidence_matrix.h"
 
 int main(){
     /* Format de fichier :
@@ -15,14 +19,14 @@ int main(){
     *
     * ordre des connexions non important
     */
-
-    FILE *f;
-    char filename[50];
-    int file_id = 1;
-
-    // Trouver un nom de fichier qui n'existe pas encore
-    do {
-        sprintf(filename, "gen_graph%d.txt", file_id);
+   
+   FILE *f;
+   char filename[50];
+   int file_id = 1;
+   
+   // Trouver un nom de fichier qui n'existe pas encore
+   do {
+       sprintf(filename, "gen_graph%d.txt", file_id);
         f = fopen(filename, "r");
         if(f != NULL) {
             fclose(f);
@@ -48,10 +52,9 @@ int main(){
     
     int max_dist = 10, max_capa = 10;
 
-    int rand_i = rand() % max_size+1;
     int min_lines = 1, lines = 0;
     for(int i = 0; i < rand_size; i++){
-        for(int ii = 0; ii < rand_size -1; ii++){
+        for(int ii = 0; ii < rand_size; ii++){
             if(i != ii && ((rand()%link_chance) == 0 || lines < min_lines)){
                 fprintf(f, "%d %d %d %d\n", nodes[i], nodes[ii], rand()%max_dist+1, rand()%max_capa+1);
                 lines++;
@@ -59,8 +62,32 @@ int main(){
             }
         }// printf("\n");
     }
-
+    
     fclose(f);
+
+    //----------------------
+
+    Variables* var = init_variables();
+    Matrix* mat = init_matrix_from_file(var, filename);
+
+    f = fopen(filename, "w");
+    if(f == NULL) {printf("Erreur lors de la création du fichier.\n"); return 1;}
+
+    explore_all_nodes_width(mat);
+    for(int i = 0; i < mat->size; i++){
+        if(mat->nodes[i]->explored == false){
+            int a = rand()%rand_size;
+            while(a == i || mat->nodes[a] == false) a = rand()%rand_size;
+
+            fprintf(f, "%d %d %d %d\n", nodes[a], nodes[i], rand()%max_dist+1, rand()%max_capa+1);
+        }
+    }
+
+    fclose(f);    
+
+    free_matrix_content(mat);
+    free_matrix(mat);
+    free_variables_struct(var);
     return 0;
 }
 

0bb7c946e2db39bac528e700cb7d59d77c9692be
nono2945 <coupriejoseph@gmail.com>
2025-05-11 13:53:59 +0200

    file_gen

--- test/gen_files/file_gen.c
+++ test/gen_files/file_gen.c
@@ -42,14 +42,9 @@ int main(){
     int max_size = 15, min_size = 5, link_chance = (max_size-1) / 3;
     // rand_size = 127; MAX
     int rand_size = (rand() % max_size);
-    // printf("-- rand_size = %d\n", rand_size);
+    // printf("--- rand_size = %d\n", rand_size);
     if(rand_size < min_size) rand_size = min_size;
     fprintf(f, "%d\n", rand_size);
-    
-    char nodes[rand_size+1];
-    for(int i = 0; i < rand_size; i++){
-        nodes[i] = i+1; // nodes[i] = 'A' + i; // printf("%c ", nodes[i]);
-    }// printf("\n");
     
     int max_dist = 10, max_capa = 10;
 
@@ -57,7 +52,7 @@ int main(){
     for(int i = 0; i < rand_size; i++){
         for(int ii = 0; ii < rand_size; ii++){
             if(i != ii && ((rand()%link_chance) == 0 || lines < min_lines)){
-                fprintf(f, "%d %d %d %d\n", nodes[i], nodes[ii], rand()%max_dist+1, rand()%max_capa+1);
+                fprintf(f, "%d %d %d %d\n", i+1, ii+1, rand()%max_dist+1, rand()%max_capa+1);
                 lines++;
                 // printf("%c - %c | ", nodes[i], nodes[ii]);
             }
@@ -72,7 +67,14 @@ int main(){
     Matrix* mat = init_matrix_from_file(var, filename);
 
     f = fopen(filename, "a");
-    if(f == NULL) {printf("\033[0;31mERROR oppening file\033[0m\n"); return 1;}
+    if(f == NULL) {
+        printf("\033[0;31mERROR oppening file\033[0m\n"); 
+        
+        free_matrix_content(mat);
+        free_matrix(mat);
+        free_variables_struct(var);
+        return 1;
+    }
     // fprintf(f, "1 1 1 1\n");
 
     explore_all_nodes_width(mat);
@@ -81,12 +83,12 @@ int main(){
             int a = rand()%rand_size;
             while(a == i || mat->nodes[a]->explored == false) a = rand()%rand_size;
             printf("\033[1;35mFILE : \033[0madded line  %d %d %d %d\n", 
-                nodes[a], nodes[i], rand()%max_dist+1, rand()%max_capa+1);
-            fprintf(f, "%d %d %d %d\n", nodes[a], nodes[i], rand()%max_dist+1, rand()%max_capa+1);
+                a+1, i+1, rand()%max_dist+1, rand()%max_capa+1);
+            fprintf(f, "%d %d %d %d\n", a+1, i, rand()%max_dist+1, rand()%max_capa+1);
         }
     }
 
-    fclose(f);    
+    fclose(f);
 
     free_matrix_content(mat);
     free_matrix(mat);
